f1a4302fa85420964dad9994147cb3d6e5dcf987
ttt20222 <ju83144@gmail.com>
2024-07-17 15:29:51 +0900

    test:list service listMoving

--- src/list/list.service.spec.ts
+++ src/list/list.service.spec.ts
@@ -9,6 +9,7 @@ import { User } from 'src/user/entities/user.entity';
 import { LexoRank } from 'lexorank';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUserMemberGuard } from 'src/auth/guards/current-user-member-auth.guard';
+import { UpdateOrderDto } from './dto/update-order.dto';
 
 describe('ListService', () => {
   let service: ListService;
@@ -21,6 +22,7 @@ describe('ListService', () => {
       {
         provide: getRepositoryToken(List),
           useValue: {
+            find: jest.fn(),
             findOne: jest.fn(),
             save: jest.fn(),
       },
@@ -119,6 +121,147 @@ describe('ListService', () => {
       const result = await service.createList(user, createListDto);
       expect(result).toEqual(newList);
       expect(result.rank).toBe(LexoRank.parse(existingList.rank).genNext().toString());
+    });
+  });
+
+  //리스트 순서 이동
+  describe('updateListOrder', () => {
+    it('should throw BadRequestException if location that cannot be moved', async () => {
+      const updateOrderDto: UpdateOrderDto = {
+        boardId: 1,
+        listId: 2,
+        sort: 3
+      };
+
+      const user = new User();
+      const lists: List[] = [
+        { boardId: 1, listId: 1, title: 'List 1', rank: '0|hzzzzr:', createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+        { boardId: 1, listId: 2, title: 'List 2', rank: '0|hzzzzz:' , createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+      ];
+
+      
+      listRepository.find.mockResolvedValueOnce(lists)
+
+      await expect(service.updateListOrder(user, updateOrderDto)).rejects.toThrow(BadRequestException);
+    });
+
+    it('should throw BadRequestException if list does not exist', async () => {
+      const updateOrderDto: UpdateOrderDto = {
+        boardId: 1,
+        listId: 3,
+        sort: 1
+      };
+
+      const user = new User();
+      const lists: List[] = [
+        { boardId: 1, listId: 1, title: 'List 1', rank: '0|hzzzzr:', createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+        { boardId: 1, listId: 2, title: 'List 2', rank: '0|hzzzzz:' , createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+      ];
+      
+      listRepository.find.mockResolvedValueOnce(lists)
+
+      await expect(service.updateListOrder(user, updateOrderDto)).rejects.toThrow(BadRequestException);
+    });
+
+    it('should update the order of the list - Moving to the beginning', async () => {
+      const updateOrderDto: UpdateOrderDto = {
+        boardId: 1,
+        listId: 2,
+        sort: 1
+      };
+
+      const user = new User();
+      const lists: List[] = [
+        { boardId: 1, listId: 1, title: 'List 1', rank: '0|hzzzzr:', createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+        { boardId: 1, listId: 2, title: 'List 2', rank: '0|hzzzzz:' , createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+      ];
+
+      listRepository.find.mockResolvedValueOnce(lists);
+
+      const firstList = lists[0];
+      const movingList = { ...lists[1] };
+      movingList.rank = LexoRank.parse(firstList.rank).genPrev().toString();
+
+      listRepository.save.mockResolvedValueOnce(movingList);
+      
+      const result = await service.updateListOrder(user, updateOrderDto);
+
+      expect(result).toEqual(movingList);
+      expect(result.rank).toBe(LexoRank.parse(firstList.rank).genPrev().toString());
+      expect(listRepository.find).toHaveBeenCalledWith({
+        where: { boardId: updateOrderDto.boardId },
+        order: { rank: 'ASC' },
+      });
+      expect(listRepository.save).toHaveBeenCalledWith(movingList);
+    });
+
+    it('should update the order of the list - Inserting in the middle', async () => {
+      const updateOrderDto: UpdateOrderDto = {
+        boardId: 1,
+        listId: 3,
+        sort: 2
+      };
+
+      const user = new User();
+      const lists: List[] = [
+        { boardId: 1, listId: 1, title: 'List 1', rank: '0|hzzzzr:', createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+        { boardId: 1, listId: 2, title: 'List 2', rank: '0|hzzzzt:' , createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+        { boardId: 1, listId: 3, title: 'List 3', rank: '0|hzzzzz:' , createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+      ];
+
+      listRepository.find.mockResolvedValueOnce(lists);
+
+      const beforeList = lists[updateOrderDto.sort-2];
+      const afterList = lists[updateOrderDto.sort-1];
+      const beforeRank = LexoRank.parse(beforeList.rank);
+      const afterRank = LexoRank.parse(afterList.rank);
+      const movingList = { ...lists[lists.length-1] };
+      movingList.rank = beforeRank.between(afterRank).toString();
+
+      listRepository.save.mockResolvedValueOnce(movingList);
+      
+      const result = await service.updateListOrder(user, updateOrderDto);
+
+      expect(result).toEqual(movingList);
+      expect(result.rank).toBe(beforeRank.between(afterRank).toString());
+      expect(listRepository.find).toHaveBeenCalledWith({
+        where: { boardId: updateOrderDto.boardId },
+        order: { rank: 'ASC' },
+      });
+      expect(listRepository.save).toHaveBeenCalledWith(movingList);
+
+    });
+
+    it('should update the order of the list - Moving to the end', async () => {
+      const updateOrderDto: UpdateOrderDto = {
+        boardId: 1,
+        listId: 1,
+        sort: 2
+      };
+
+      const user = new User();
+      const lists: List[] = [
+        { boardId: 1, listId: 1, title: 'List 1', rank: '0|hzzzzr:', createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+        { boardId: 1, listId: 2, title: 'List 2', rank: '0|hzzzzz:' , createdAt: new Date(), updatedAt: new Date(), board: null, cards: []} as List,
+      ];
+
+      listRepository.find.mockResolvedValueOnce(lists);
+
+      const lastList = lists[lists.length-1];
+      const movingList = { ...lists[0] };
+      movingList.rank = LexoRank.parse(lastList.rank).genNext().toString();
+
+      listRepository.save.mockResolvedValueOnce(movingList);
+      
+      const result = await service.updateListOrder(user, updateOrderDto);
+
+      expect(result).toEqual(movingList);
+      expect(result.rank).toBe(LexoRank.parse(lastList.rank).genNext().toString());
+      expect(listRepository.find).toHaveBeenCalledWith({
+        where: { boardId: updateOrderDto.boardId },
+        order: { rank: 'ASC' },
+      });
+      expect(listRepository.save).toHaveBeenCalledWith(movingList);
     });
   });
 });
